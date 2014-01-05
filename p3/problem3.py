0f42d9c56b9e872370f49b9acc3e9221642d15d9
djmittens <nick.meister37@gmail.com>
2014-01-04 23:57:31 -0700

    Finished up the third problem

--- p3/problem3.py
+++ p3/problem3.py
@@ -1,18 +1,12 @@
-from multiprocessing import Pool
-
-def prime(num):
-    if num == 1:
-        return False
-    if num % 2 == 0:
-        return False
-    for i in xrange(3, num / 2, 1) :
-        if num % i == 0:
-            return False
-    return True
-pool = Pool(4)
 N = 600851475143
 
 def factors(n):    
     return set(reduce(list.__add__, 
                 ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
-print factors(N)
+
+def prime(num):
+    if len(factors(num)) > 2:
+        return False
+    return True
+
+print sorted(filter(prime, factors(N))) [-1]
