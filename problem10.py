5b0fa070c147b52c5874e0bad018e4c3d9f5dd06
djmittens <nick.meister37@gmail.com>
2014-01-05 23:09:04 -0700

    Started working on problem 11

---
+++ problem10.py
@@ -0,0 +1,3 @@
+from common import *
+
+print sum(filter(prime, range(2, 2000000)))
--- problem9.py
+++ problem9.py
@@ -1,4 +1,3 @@
 from common import *
 
-for i in xrange(2, 1000, 2):
-    print map(lambda x: x[0] * x[1] * x[2], filter( lambda x: x[0] + x[1] + x[2] == 1000, map(lambda x: [x[0]**2 - x[1]**2, 2 * x[0] * x[1], x[0]**2 + x[1]**2], map(lambda x: [x, i / x], factors(i)))))
+print filter(lambda x: len(x) > 0 and x[0] > 0, [map(lambda x: x[0] * x[1] * x[2], filter( lambda x: x[0] + x[1] + x[2] == 1000, map(lambda x: [x[0]**2 - x[1]**2, 2 * x[0] * x[1], x[0]**2 + x[1]**2], map(lambda x: [x, i / x], factors(i))))) for i in xrange(2, 1000, 2)])
